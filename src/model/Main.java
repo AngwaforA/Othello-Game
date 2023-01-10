a035a9f21a0e0c07ace9d22e9b165bb181cadbd7
AngwaforA <splashashley@gmail.com>
2023-01-10 09:14:45 +0100

    addition of gameOver and winner

---
+++ src/model/Main.java
@@ -0,0 +1,36 @@
+package model;
+
+public class Main {
+    public static void main(String[] args) {
+        GameBoard gameBoard = new GameBoard();
+        gameBoard.iterateGameBoardForFreeMove(2);
+        gameBoard.cpuMove();
+        System.out.println("winner is" + gameBoard.getWinner());
+        gameBoard.getWinner();
+
+    }
+                    /*if (horizontalBackwardFreeMove(i, chip)) {
+                    System.out.println("pos: " + i + "horizontalBackwardFreeMove:" + freeMove);
+                }
+                if (horizontalForwardFreeMove(i, chip)) {
+                    System.out.println("pos: " + i + "horizontalForwardFreeMove:" + freeMove);
+                }
+                if (verticalBackwardFreeMove(i, chip)) {
+                    System.out.println("pos: " + i + " verticalBackwardFreeMove:" + freeMove);
+                }
+                if (verticalForwardFreeMove(i, chip)) {
+                    System.out.println("pos: " + i + "verticalForwardFreeMove:" + freeMove);
+                }
+                if (diagonalForwardRightFreeMove(i, chip)) {
+                    System.out.println("pos: " + i + "diagonalForwardRightFreeMove:" + freeMove);
+                }
+                if (diagonalBackwardRightFreeMove(i, chip)) {
+                    System.out.println("pos: " + i + "diagonalBackwardRightFreeMove:" + freeMove);
+                }
+                if (diagonalBackwardLeftFreeMove(i, chip)) {
+                    System.out.println("pos: " + i + "diagonalBackwardLeftFreeMove:" + freeMove);
+                }
+                if (diagonalForwardLeftFreeMove(i, chip)) {
+                    System.out.println("pos: " + i + "diagonalForwardLeftFreeMove:" + freeMove);
+                }*/
+}
